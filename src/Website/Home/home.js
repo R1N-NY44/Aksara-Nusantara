d170c3bc85cecbf2031a9cf2561960e340c611a5
Gede Astu Nugraha <gede.astu@gmail.com>
2024-05-21 13:04:20 +0700

    feat: fixed sticky nav

--- src/Website/Home/home.js
+++ src/Website/Home/home.js
@@ -1,19 +1,37 @@
-const body = document.body
-let lastScroll = 0
+// const body = document.body
+// let lastScroll = 0
+// const header = document.getElementById('header')
 
-window.addEventListener("scroll", () => {
-    const currentScroll = window.scrollY;
-    if (currentScroll <= 0){
-        body.classList.remove("scroll-up");
-        return;
-    }
-    if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
-        body.classList.remove("scroll-up");
-        body.classList.add("scroll-down");
-    }else if(currentScroll < lastScroll && body.classList.contains("scroll-down")){
-        body.classList.remove("scroll-down");
-        body.classList.add("scroll-up");
-    }
-    lastScroll = currentScroll
-    console.log(lastScroll)
+const { default: Alpine } = require("alpinejs")
+
+// window.addEventListener("scroll", () => {
+//     const currentScroll = window.scrollY;
+//     if (currentScroll <= 0){
+//         body.classList.add("scroll-up");
+//         header.classList.remove("shadow-md")
+//         return;
+//     }
+//     console.log(currentScroll)
+//     if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+//         body.classList.remove("scroll-up");
+//         body.classList.add("scroll-down");
+//     }else if(currentScroll < lastScroll && body.classList.contains("scroll-down")){
+//         body.classList.remove("scroll-down");
+//         body.classList.add("scroll-up");
+//         header.classList.add("shadow-md")
+//     }
+//     lastScroll = currentScroll
+//     console.log(lastScroll)
+// })
+document.addEventListener("alpine:init",()=>{
+    Alpine.data('stickyNavbar',()=>({
+        hideNavbar:false,
+        lastScrollTop:0,
+        handleScroll(){
+            let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            this.hideNavbar = scrollTop > this.lastScrollTop;
+            this.lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+        }
+    }))
 })
+
